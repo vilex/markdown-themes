be0922f7e12d539c10480e25ee15e403952efd1f
yanjinjin <yanjinjin@100doc.com.cn>
2022-11-09 18:44:39 +0800

    feat: 代码块高亮

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
-import MarkdownIt from 'markdown-it'
-import MdFile from './files/component.md'
 import './theme/index.scss'
+// import 'highlight.js/styles/default.css'
 
-const mi = new MarkdownIt()
+import MarkdownIt from 'markdown-it'
+import hljs from 'highlight.js'
+import MdFile from './files/component.md'
+
+
+const md = new MarkdownIt({
+    highlight: (content, lang) => {
+        if (lang && hljs.getLanguage(lang)) {
+            return `<pre class="hljs"><code>${hljs.highlight(lang, content, true).value}</code></pre>`
+        }
+    }
+})
 
 const result = await fetch(MdFile)
 const content = await result.text()
 
 const app = document.querySelector('#app')!
 app.className = 'markdown-theme'
-app.innerHTML = mi.render(content)
+app.innerHTML = md.render(content)
